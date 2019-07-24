aca4cf41d950d9194238538d1dc8aea26c01dba0
hejin <1179767901@qq.com>
2019-07-24 16:44:09 +0800

    add babel stage-2

--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,7 @@
 // import { Base64 } from "../encrypt/base64";
 // console.log(Base64);
 
-import Utils from '../dist/utils.min'
+import Utils from "../dist/utils.min";
 console.log(Utils);
 
 import { Md5 } from "../index";
